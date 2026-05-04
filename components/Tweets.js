5ad33bb2c9d67ea624f0dcbc1f66b4a321df7d39
kd-com <contact@kd-com.fr>
2026-05-04 09:58:38 +0200

    ADD - feed component

--- components/Tweets.js
+++ components/Tweets.js
@@ -2,11 +2,14 @@ import styles from "../styles/Tweets.module.scss";
 import DynamicButton from "./DynamicButton";
 import { useState } from "react";
 import LeftSide from "./LeftSide";
+import Feed from "./feed";
 
 function Tweetsdisplay() {
     return (
         <div className={styles.pageContainer}>
             <LeftSide />
+            <Feed />
+
 
         </div>
     )
